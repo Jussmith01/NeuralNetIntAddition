e451bd49a80f3e710aa82b5041dc65ed7bf8016c
Justin Smith <jussmith48@gmail.com>
2015-08-08 11:16:36 -0400

    Added file

---
+++ src/tools/csvreader.hpp
@@ -0,0 +1,22 @@
+#ifndef CSVREADER_C
+#define CSVREADER_C
+
+#include <vector>
+#include <bitset>
+#include <limits.h>
+#include <math.h>
+#include <cstring>
+#include <string>
+#include <iostream>
+
+/*----------------------------------------
+  ***************************************
+  |  read and store a CSV  |
+  ***************************************
+------------------------------------------*/
+void csvreader(std::string filename,std::vector< std::vector<float> > &data,int colstart, int colend)
+{
+	ifstream 	
+};
+
+#endif
